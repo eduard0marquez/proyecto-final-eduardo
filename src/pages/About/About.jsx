bd67e70426999959511ee36569b3d3326cb98d53
emarqueztmr <edwartmarquez@gmail.com>
2025-06-17 07:30:04 -0600

    Nosotros/catalogo categorias

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Col, Container, Image, Row } from "react-bootstrap";
 import { TbBackground } from "react-icons/tb";
-import"./About.css"
+import "./About.css";
 function About() {
   return (
     <Container style={{ marginTop: 100 }}>
@@ -22,7 +22,9 @@ function About() {
         </p>
 
         <p className=" text-center ">
-          ¡Equipate con nuestros articulos e impulsa tus ideas!
+          <strong>
+            <i>¡Equipate con nuestros articulos e impulsa tus ideas!</i>
+          </strong>
         </p>
         <br />
         <h3 className="whit-des text-center mb-5">Desarrolladores</h3>
@@ -30,9 +32,9 @@ function About() {
           <div className="flip-card">
             <div className="flip-card-inner">
               <div className="flip-card-front">
-                <Image className="foto"
+                <Image
+                  className="foto"
                   src="https://scontent.fmex10-2.fna.fbcdn.net/v/t39.30808-6/480519534_9392905714150847_8499012358417627994_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeHXVCjD3fA0aSJrpIGRBLtnJb1ouquoMJQlvWi6q6gwlOCBzXVH0vd8LzQukrOz_0C_X1T_zNPovfuWrG-rPz_b&_nc_ohc=nuY5U65seBgQ7kNvwEpiWpy&_nc_oc=AdkWntoI7hIK1RybrCuwAiiJ8PdZ6DTuV-08lRQsigAsyw30BPmM2iMh0zLADYmUJSzlo9stB7g0dq9a2ExJn93O&_nc_zt=23&_nc_ht=scontent.fmex10-2.fna&_nc_gid=YdEIs_RbndrPI7xwk_u7eQ&oh=00_AfNenBMUCY5AVtv7uddWhOF9WOHyri9TZvkrJ6agZWE4Ew&oe=68574DB6"
-                  
                   roundedCircle
                 />
                 <h4>Eduardo Marquez</h4>
@@ -41,7 +43,6 @@ function About() {
                 <h1>Eduardo Marquez</h1>
                 <p>Ocupacion: Desarrollador</p>
                 <p>Edad: 29 años</p>
-                
               </div>
             </div>
           </div>
